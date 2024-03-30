3c463a6e069956ea7a3a163e8aa3cac437289a6b
nihongoo <127075883+nihongoo@users.noreply.github.com>
2024-03-30 12:15:03 +0700

    có sai ba cx vip nhưng footer hơi tật (có thêm file style.css + viết thêm js)

--- App_MVC/wwwroot/js/site.js
+++ App_MVC/wwwroot/js/site.js
@@ -1,4 +1,12 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿window.addEventListener('DOMContentLoaded', event => {
+    // Toggle the side navigation
+    const sidebarToggle = document.body.querySelector('#sidebarToggle');
+    if (sidebarToggle) {
+        sidebarToggle.addEventListener('click', event => {
+            event.preventDefault();
+            document.body.classList.toggle('sb-sidenav-toggled');
+            localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
+        });
+    }
 
-// Write your JavaScript code.
+});
