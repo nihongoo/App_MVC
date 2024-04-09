d806e6e855d6e3728ec83777e8652488b81dc031
nihongoo <127075883+nihongoo@users.noreply.github.com>
2024-04-09 08:00:49 +0700

    commit ngay di trc moi dieu dan toi te hon

--- App_MVC/wwwroot/js/site.js
+++ App_MVC/wwwroot/js/site.js
@@ -10,35 +10,3 @@
     }
 
 });
-//script trang giỏ hàng chi tiết
-// function increaseQuantity(productId) {
-//     var quantityElement = document.getElementById("quantity_" + productId);
-//     var hiddenQuantityElement = document.getElementById("hiddenQuantity_" + productId);
-//     var totalElement = document.getElementById("total_" + productId);
-
-//     var quantity = parseInt(quantityElement.innerText);
-//     quantityElement.innerText = quantity + 1;
-//     hiddenQuantityElement.value = quantity + 1;
-
-//     updateTotal(productId);
-// }
-
-// function decreaseQuantity(productId) {
-//     var quantityElement = document.getElementById("quantity_" + productId);
-//     var hiddenQuantityElement = document.getElementById("hiddenQuantity_" + productId);
-//     var totalElement = document.getElementById("total_" + productId);
-
-//     var quantity = parseInt(quantityElement.innerText);
-//     if (quantity > 1) {
-//         quantityElement.innerText = quantity - 1;
-//         hiddenQuantityElement.value = quantity - 1;
-//         updateTotal(productId);
-//     }
-// }
-
-// function updateTotal(productId) {
-//     var quantity = parseInt(document.getElementById("quantity_" + productId).innerText);
-//     var price = parseFloat(document.getElementById("price_" + productId).innerText.replace(" đ", ""));
-//     var total = quantity * price;
-//     document.getElementById("total_" + productId).innerText = total.toLocaleString('en-US') + " đ";
-// }
